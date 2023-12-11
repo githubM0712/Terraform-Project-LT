777bba565183b80aa9abb4a5ea1b0b00d4169abb
githubM0712 <136787794+githubM0712@users.noreply.github.com>
2023-12-11 18:07:14 +0530

    Update main.tf

--- modules/rds_instance/main.tf
+++ modules/rds_instance/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "my_db_subnet_group" {
-  name       = var.aws_db_subnet_group_name
+  name       = var.db_subnet_group_name
   subnet_ids = var.private_subnet.ids
 
   tags = {
