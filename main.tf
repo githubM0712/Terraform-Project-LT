1bb1f5687c8cdc620d5ce1139043ba24b8ed9e9a
githubM0712 <136787794+githubM0712@users.noreply.github.com>
2023-12-11 17:18:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   source               = "./modules/vpc"
   vpc_cidr             = var.vpc_cidr
+  region               = var.region
   public_subnets_cidr  = var.public_subnets_cidr
   private_subnets_cidr = var.private_subnets_cidr
   availability_zones   = var.availability_zones
